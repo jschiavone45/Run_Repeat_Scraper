46df63358125f9d67c838ff56a0370270517c307
Jack Schiavone <jschiavone.js@gmail.com>
2020-07-23 21:31:10 -0400

    scraper working properly, need to write to dict to csv

--- run_repeat_scraper.py
+++ run_repeat_scraper.py
@@ -35,16 +35,21 @@ for url in url_list:
     for i in range(len(product_links)):
         driver.get(product_links[i])
 
-        #driver.execute_script("window.scrollTo(0, 1700);")
-
+        driver.execute_script("window.scrollTo(0, 1900);")
+        time.sleep(1)
         #clicking to expand table
-        #more_facts_button = driver.find_element_by_class_name("div.btn btn-expand btn-default")
-        #more_facts_button.click()
+        try:
+            more_facts_button = driver.find_element_by_xpath('//div[@class="btn-more-container"]')
+            more_facts_button.click()
+        except:
+            pass
 
+        time.sleep(3)
         #scraping table from page and converting to a dictionary
+
+
         table = driver.find_elements_by_xpath('//table[@class="table table-striped"]/tbody/tr')
         table = [x.text for x in table]
-        print(table)
         spec_dict = {}
         #print(table)
         for row in table:
@@ -53,42 +58,18 @@ for url in url_list:
             except:
                 pass
             spec_dict[feature] = spec
+
+
+
+        spec_dict["product"] = driver.find_element_by_xpath('//*[@id="product-title"]/h1/span').text
+        spec_dict["core score"] = int(driver.find_element_by_xpath('//*[@id="corescore"]/span').text)
+        spec_dict["reviews"] = int(re.findall('\d+', driver.find_element_by_xpath('//*[@id="rr-top-section"]/div[2]/div[1]/div[2]/div/span[2]/button').text)[0])
+        spec_dict["expert_score"] = int(driver.find_element_by_xpath('//*[@id="experts_reviews_section"]/div/p').text[0:2])
+        spec_dict["summary"] = driver.find_element_by_xpath('//*[@id="bottom_line_section"]/div[2]').text
+
         print('*'*50)
         print(spec_dict)
         print('*'*50)
-
-
-        #product = driver.find_element_by_xpath('//*[@id="product-title"]/h1/span').text
-        #brand = driver.find_element_by_xpath('//*[@id="rr_facts"]/div/div[1]/div[1]/table/tbody/tr[14]/td').text
-        #price = driver.find_element_by_xpath('//*[@id="rr_facts"]/div/div[1]/div[1]/table/tbody/tr[16]/td/span/span/span').text
-        #sale_price = driver.find_element_by_xpath('//*[@id="rr_facts"]/div/div[1]/div[1]/table/tbody/tr[16]/td/span/span/button').text
-        #core_score = driver.find_element_by_xpath('//*[@id="corescore"]/span').text
-        #reviews = driver.find_element_by_xpath('//*[@id="rr-top-section"]/div[2]/div[1]/div[2]/div/span[2]/button').text
-        #expert_score = driver.find_element_by_xpath('//*[@id="experts_reviews_section"]/div/p').text
-        #summary = driver.find_element_by_xpath('//*[@id="bottom_line_section"]/div[2]').text
-        #terrain = driver.find_element_by_xpath('//*[@id="rr_facts"]/div/div[1]/div[1]/table/tbody/tr[1]/td').text
-        #arch_support = driver.find_element_by_xpath('//*[@id="rr_facts"]/div/div[1]/div[1]/table/tbody/tr[2]/td').text
-        #mens_weight = driver.find_element_by_xpath('//*[@id="rr_facts"]/div/div[1]/div[1]/table/tbody/tr[3]/td/span/span/span[1]/span[1]').text
-        #womens_weight = driver.find_element_by_xpath('//*[@id="rr_facts"]/div/div[1]/div[1]/table/tbody/tr[3]/td/span/span/span[2]/span').text
-        #heel_to_toe_men =
-        #heel_to_toe_women =
-        #fit =
-        #pronation =
-        #arch_type =
-        #use =
-        #material =
-        #features =
-        #release_date =
-        #width =
-        #print(product)
-        #print(terrain)
-        #print(brand)
-        #print(price)
-        #print(sale_price)
-        #print(core_score)
-        #print(reviews)
-        #print(expert_score)
-
 
         # Unpack spec_dict into item fields
         #item['terrain'] = spec_dict.get('Terrain')
