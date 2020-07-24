460fb27d8c933ecdae077c95bf9bc6f57e0225a4
Jack Schiavone <jschiavone.js@gmail.com>
2020-07-24 11:45:49 -0400

    first run of scraper, need to debug and rerun

--- run_repeat_scraper.py
+++ run_repeat_scraper.py
@@ -4,6 +4,13 @@ import re
 import csv
 import math
 
+
+#set up to write to csv_file
+csv_file = open('running_shoes2.csv', 'w', encoding='utf-8', newline='')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['product', 'brand', 'core_score', 'expert_score', 'reviews', 'retail_price', 'sale_price',
+                    'terrain', 'arch_support', 'weight_men', 'weight_women', 'heel_to_toe_men', 'heel_to_toe_women', 'release_date', 'width', 'update', 'summary'])
+
 driver = webdriver.Chrome()
 driver.get("https://runrepeat.com/ranking/rankings-of-running-shoes?page=1")
 
@@ -13,7 +20,7 @@ driver.get("https://runrepeat.com/ranking/rankings-of-running-shoes?page=1")
 num_products = driver.find_element_by_xpath('//*[@id="rankings-header"]/h1').text
 num_products = int(re.findall('\d+', num_products)[0])
 total_pages = math.ceil(num_products / 30)
-url_list = [f'https://runrepeat.com/ranking/rankings-of-running-shoes?page={i + 1}' for i in range(total_pages)]
+url_list = [f'https://runrepeat.com/ranking/rankings-of-running-shoes?page={i + 3}' for i in range(total_pages)]
 
 
 product_links = driver.find_elements_by_xpath('//div[@class="product-name hidden-sm hidden-xs"]/a')
@@ -21,11 +28,11 @@ product_links = [x.get_attribute('href') for x in product_links]
 
 
 
-
+page_num = 1
 
 #begin cycle through each url
 for url in url_list:
-
+    print(f'Scraping page {page_num}')
     # create list of product links to click for current page
     driver.get(url)
     product_links = driver.find_elements_by_xpath('//div[@class="product-name hidden-sm hidden-xs"]/a')
@@ -36,7 +43,7 @@ for url in url_list:
         driver.get(product_links[i])
 
         driver.execute_script("window.scrollTo(0, 1900);")
-        time.sleep(1)
+        time.sleep(2)
         #clicking to expand table
         try:
             more_facts_button = driver.find_element_by_xpath('//div[@class="btn-more-container"]')
@@ -44,10 +51,10 @@ for url in url_list:
         except:
             pass
 
-        time.sleep(3)
+        time.sleep(2)
+
+
         #scraping table from page and converting to a dictionary
-
-
         table = driver.find_elements_by_xpath('//table[@class="table table-striped"]/tbody/tr')
         table = [x.text for x in table]
         spec_dict = {}
@@ -58,28 +65,67 @@ for url in url_list:
             except:
                 pass
             spec_dict[feature] = spec
+            # checking to see what data is missing
+        if 'Update' not in spec_dict:
+            spec_dict['Update'] = 'NaN'
+        if 'Release date' not in spec_dict:
+            spec_dict['Release date'] = 'NaN'
+        if 'Brand' not in spec_dict:
+            spec_dict['Brand'] = 'NaN'
+        if 'Width' not in spec_dict:
+            spec_dict['Width'] = 'NaN'
+        if 'Arch support' not in spec_dict:
+            spec_dict['Arch support'] = 'NaN'
+        if 'Terrain' not in spec_dict:
+            spec_dict['Terrain'] = 'NaN'
+        if 'Price' not in spec_dict:
+            retail_price = 'NaN'
+            sale_price = 'NaN'
+        try:
+            product = driver.find_element_by_xpath('//*[@id="product-title"]/h1/span').text
+        except:
+            product = "NaN"
+        try:
+            core_score = int(driver.find_element_by_xpath('//*[@id="corescore"]/span').text)
+        except:
+            core_score = 'NaN'
+        try:
+            reviews = int(re.findall('\d+', driver.find_element_by_xpath('//*[@id="rr-top-section"]/div[2]/div[1]/div[2]/div/span[2]/button').text)[0])
+        except:
+            reviews = 'NaN'
+        try:
+            expert_score = int(driver.find_element_by_xpath('//*[@id="experts_reviews_section"]/div/p').text[0:2])
+        except:
+            expert_score = 'NaN'
+        try:
+            summary = driver.find_element_by_xpath('//*[@id="bottom_line_section"]/div[2]').text
+        except:
+            summary = 'NaN'
+        try:
+            price_list = spec_dict['Price'].strip().split(' ', 1)
+            retail_price, sale_price = price_list
+        except:
+            try:
+                retail_price = spec_dict['Price']
+                sale_price = 'NaN'
+            except:
+                retail_price = 'NaN'
+                sale_price = 'NaN'
+
+        try:
+            weight_men, weight_women = spec_dict['Weight'].split('|')
+        except:
+            weight_men, weight_women = 'NaN', 'NaN'
+        try:
+            heel_to_toe_men, heel_to_toe_women = spec_dict['Heel to toe drop'].split('|')
+        except:
+            heel_to_toe_men, heel_to_toe_women = 'NaN', 'NaN'
 
 
-
-        spec_dict["product"] = driver.find_element_by_xpath('//*[@id="product-title"]/h1/span').text
-        spec_dict["core score"] = int(driver.find_element_by_xpath('//*[@id="corescore"]/span').text)
-        spec_dict["reviews"] = int(re.findall('\d+', driver.find_element_by_xpath('//*[@id="rr-top-section"]/div[2]/div[1]/div[2]/div/span[2]/button').text)[0])
-        spec_dict["expert_score"] = int(driver.find_element_by_xpath('//*[@id="experts_reviews_section"]/div/p').text[0:2])
-        spec_dict["summary"] = driver.find_element_by_xpath('//*[@id="bottom_line_section"]/div[2]').text
-
-        print('*'*50)
-        print(spec_dict)
-        print('*'*50)
-
-        # Unpack spec_dict into item fields
-        #item['terrain'] = spec_dict.get('Terrain')
-        #item['updated_prod'] = spec_dict.get('Update', 'No product')
-
-        time.sleep(1)
+        # writing next row of csv file with current product_page
+        write_row = [product, spec_dict['Brand'], core_score, expert_score, reviews, retail_price, sale_price, spec_dict['Terrain'],
+                    spec_dict['Arch support'], weight_men, weight_women, heel_to_toe_men, heel_to_toe_women, spec_dict['Release date'], spec_dict['Width'], spec_dict['Update'], summary]
+        csv_writer.writerow(write_row)
 
 
-#    #page_num = 1
-
-#while True:
-    #print(f'Scraping page {page_num}')
-    #products = driver.find_elements_by_xpath('.//div[@class="product-name hidden-sm hidden-xs"]/a/@href')
+    page_num +=1
